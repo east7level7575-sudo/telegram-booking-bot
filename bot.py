9c2da2950012de1fa6937457add542bcdcdcff17
east7level7575-sudo <east7level7575@gmail.com>
2025-09-16 17:31:31 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
-import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+import os
 
-# Твой токен и Telegram ID можно оставить здесь для теста, но лучше через Render Environment Variables
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8111919704:AAEtOLqEEEEVJGk6jXFEuXQYVdjgMlCeFMA")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "504004158"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Бот работает! Используй команду /book YYYY-MM-DD для бронирования.")
+    await message.reply("Bot is running! Use /book YYYY-MM-DD to book.")
 
 @dp.message_handler(commands=['book'])
 async def book_event(message: types.Message):
-    await message.reply("Бронирование принято!")
+    await message.reply("Booking received!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
