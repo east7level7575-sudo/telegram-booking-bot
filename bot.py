3d5609e26dbf94bb2f7addf79e6d055df24700f2
east7level7575-sudo <east7level7575@gmail.com>
2025-09-16 17:40:29 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,23 @@
+import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+API_TOKEN = "8111919704:AAEtOLqEEEEVJGk6jXFEuXQYVdjgMlCeFMA"
+ADMIN_ID = 504004158
 
-bot = Bot(token=BOT_TOKEN)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Bot is running! Use /book YYYY-MM-DD to book.")
+async def start(message: types.Message):
+    await message.reply("Бот работает! Используй /book для бронирования.")
 
 @dp.message_handler(commands=['book'])
-async def book_event(message: types.Message):
-    await message.reply("Booking received!")
+async def book(message: types.Message):
+    await message.reply("Ты сделал бронирование!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
